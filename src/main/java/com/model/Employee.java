cb36ea1a73560c7beab2fd7a52aeeeb095315cf9
ASUS-pc <alexdharma@mail.ru>
2015-01-07 13:33:13 +0300

    +  id field, hibernate mapping

--- src/main/java/com/model/Employee.java
+++ src/main/java/com/model/Employee.java
@@ -1,8 +1,33 @@
-package model;
+package com.model;
 
+import javax.persistence.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEES")
 public class Employee {
 
-   private String name;
+    @Id
+    @Column(name = "ID")
+    @SequenceGenerator(name="EMPLOYEES_SQ", sequenceName="EMPLOYEES_SQ")
+    @GeneratedValue(generator = "EMPLOYEES_SQ", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SUR_NAME")
+    private String surName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 
     public String getSurName() {
         return surName;
@@ -19,7 +44,5 @@ public class Employee {
     public void setName(String name) {
         this.name = name;
     }
-
-    private String surName;
 
 }
