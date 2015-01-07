cc80a52c19cfcd09f8621dcab8126c8c354f06c2
ASUS-pc <alexdharma@mail.ru>
2015-01-08 00:40:59 +0300

    + Payout класс

---
+++ src/main/java/com/model/Payout.java
@@ -0,0 +1,67 @@
+package com.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PAYOUTS")
+public class Payout {
+
+    @Id
+    @Column(name = "ID")
+    @SequenceGenerator(name="PAYOUTS_SQ", sequenceName="PAYOUTS_SQ")
+    @GeneratedValue(generator = "PAYOUTS_SQ", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "EMPLOYEE_ID")
+    private Long employeeId;
+
+    @Column(name = "EMPLOYEE_ID")
+    private Long initiatorId;
+
+    @Column(name = "AMOUNT")
+    private BigDecimal amount;
+
+    @Column(name = "TYPE")
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Long getInitiatorId() {
+        return initiatorId;
+    }
+
+    public void setInitiatorId(Long initiatorId) {
+        this.initiatorId = initiatorId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
