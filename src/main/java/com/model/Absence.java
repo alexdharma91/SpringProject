2b52053d965dd8967cf308ceb430537888ada389
ASUS-pc <alexdharma@mail.ru>
2015-01-07 22:40:45 +0300

    + Absence class

---
+++ src/main/java/com/model/Absence.java
@@ -0,0 +1,67 @@
+package com.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ABSENCES")
+public class Absence {
+
+    @Id
+    @Column(name = "ID")
+    @SequenceGenerator(name="ABSENCES_SQ", sequenceName="ABSENCES_SQ")
+    @GeneratedValue(generator = "ABSENCES_SQ", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "EMPLOYEE_ID")
+    private Long employeeId;
+
+    @Column(name = "START_DATE")
+    private Date startDate;
+
+    @Column(name = "END_DATE")
+    private Date endDate;
+
+    @Column(name = "TYPE")
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
