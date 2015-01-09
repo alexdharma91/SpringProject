ed20402d69a8f7d6f661cb458bfd4e3e06f78ec8
ASUS-pc <alexdharma@mail.ru>
2015-01-10 02:45:32 +0300

    + Reflector class

---
+++ src/main/java/com/service/Reflector.java
@@ -0,0 +1,50 @@
+package com.service;
+
+import com.google.common.collect.Lists;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class Reflector {
+
+    public static class ClassField{
+        private String name;
+        private String type;
+
+        public ClassField(String name, String type){
+           this.name = name;
+           this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+    public static List<ClassField> getFields(Class targetClass){
+       List result = Lists.newArrayList();
+
+       for (Field field : targetClass.getDeclaredFields()){
+           boolean required = false;
+
+           for(Annotation annotation : field.getAnnotations()){
+               if(annotation.annotationType().toString().equals("interface javax.persistence.Column")){
+                   required = true;
+               }
+           }
+
+           if(required){
+               result.add(new ClassField(field.getName(), field.getType().toString()));
+           }
+
+       }
+
+       return result;
+    }
+
+}
