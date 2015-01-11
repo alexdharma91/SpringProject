4fc1f476ebbd867a3dcce67a539b638babf18cb7
Alexander <alexdharma@mail.ru>
2015-01-12 00:49:49 +0300

    try to get params as ModelAttribute

--- src/main/java/com/controllers/EmployeeController.java
+++ src/main/java/com/controllers/EmployeeController.java
@@ -1,28 +1,42 @@
 package com.controllers;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.model.Employee;
+import com.model.State;
 import com.service.Reflector;
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.InvocationTargetException;
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/employee")
-public class EmployeeController extends AbstractController{
+@SessionAttributes("fields, test")
+public class EmployeeController extends AbstractController {
 
-    @RequestMapping(value = "show",  method =  RequestMethod.GET)
+    @RequestMapping(value = "show", method = RequestMethod.GET)
     public String show(ModelMap model) {
-        List<Employee> employees =  persistentService.getAll(Employee.class);
+        List<Employee> employees = persistentService.getAll(Employee.class);
         model.addAttribute("employees", employees);
-        model.addAttribute("active" , 0);
+        model.addAttribute("active", 0);
         model.addAttribute("title", "Employees");
         return "employee";
     }
 
-    @RequestMapping(value = "create",  method =  RequestMethod.GET)
+    @RequestMapping(value = "create", method = RequestMethod.GET)
     public String createGet(ModelMap model) {
         List<Reflector.ClassField> fields = reflector.getFields(Employee.class);
 
@@ -30,23 +44,36 @@ public class EmployeeController extends AbstractController{
         model.addAttribute("status", "creation");
         model.addAttribute("title", "Create employee");
         model.addAttribute("createText", "Создать сотрудника");
+
+        model.addAttribute("test", "test");
         model.addAttribute("fields", fields);
         return "create";
     }
 
-    @RequestMapping(value = "create",  method =  RequestMethod.POST)
-    public String createPost(ModelMap model) {
-        List<Reflector.ClassField> fields = reflector.getFields(Employee.class);
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String createPost(@ModelAttribute("fields") List<Reflector.ClassField> fields,
+                             @ModelAttribute("test") String test, SessionStatus status) {
 
-        model.addAttribute("entity", "сотрудник");
-        model.addAttribute("status", "created");
-        model.addAttribute("title", "Create employee");
-        model.addAttribute("createText", "Создать сотрудника");
-        model.addAttribute("fields", fields);
+         Map map  = Maps.newHashMap();
+
+         for(Reflector.ClassField field : fields){
+             map.put(field.getName(), field.getValue());
+         }
+
+        Employee employee = new Employee();
+
+        try {
+            BeanUtils.populate(employee, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        persistentService.save(employee);
+
+        //  model.addAttribute("fields", fields);
         return "create";
     }
-
-
-
 
 }
