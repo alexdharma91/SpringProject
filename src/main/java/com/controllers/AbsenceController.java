811a217411f90d78dbcc4c131815e542f2b6476d
Alexander <alexdharma@mail.ru>
2015-02-01 23:40:18 +0300

    + edit and create methods

--- src/main/java/com/controllers/AbsenceController.java
+++ src/main/java/com/controllers/AbsenceController.java
@@ -1,12 +1,17 @@
 package com.controllers;
 
+import com.google.common.base.Optional;
 import com.model.Absence;
+import com.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -30,18 +35,42 @@ public class AbsenceController extends AbstractController {
         return "createAbsence";
     }
 
-    @RequestMapping(value = "create", method = RequestMethod.POST)
-    public String createPost() {
+    @RequestMapping(value = "edit", method = RequestMethod.GET)
+    public String editGet(ModelMap model, HttpSession session, @RequestParam(value = "absenceId") Long absenceId) {
+        model.addAttribute("title", "absence create");
+        model.addAttribute("status", "creation");
+        model.addAttribute("createText", "Создать отсутствие");
+        session.setAttribute("absenceId", absenceId);
+        Absence absence = persistentService.get(Absence.class, absenceId);
+        model.addAttribute("absence", absence);
+        return "createAbsence";
+    }
 
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String createPost(@ModelAttribute(value = "absence") Absence absence, HttpSession session) {
+        Long absenceId = Long.valueOf(Optional.fromNullable(session.getAttribute("absenceId")).or("-1").toString());
+
+        if(!absenceId.equals(-1l)){
+            absence.setId(absenceId);
+        }
+
+        persistentService.save(absence);
         return "redirect:/absence/show";
     }
 
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public String deleteGet(@RequestParam(value = "absenceId") Long absenceId){
+        persistentService.delete(Absence.class, absenceId);
+        return "redirect:/absence/show";
+    }
 
     @ModelAttribute("absence")
     public Absence populate() {
         Absence absence = new Absence();
+        absence.setType("тип");
+        absence.setStartDate(new Date());
+        absence.setEndDate(new Date());
         return absence;
     }
-
 
 }
