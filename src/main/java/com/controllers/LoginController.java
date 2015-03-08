74b298e099d61820821436b1495c4a67fe64a4bf
Alexander <alexdharma@mail.ru>
2015-03-08 17:14:00 +0300

    +part of authentication

--- src/main/java/com/controllers/LoginController.java
+++ src/main/java/com/controllers/LoginController.java
@@ -1,14 +1,24 @@
 package com.controllers;
 
+import com.model.Employee;
+import com.service.SpringAuthenticationProvider;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.Resource;
+
 @Controller
 @RequestMapping(value = "/log")
 public class LoginController extends AbstractController {
+
+    @Resource
+    SpringAuthenticationProvider authenticationProvider;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(){
@@ -22,10 +32,24 @@ public class LoginController extends AbstractController {
     }
 
     @RequestMapping(value = "in", method = RequestMethod.POST)
-    public String loginPost(ModelMap model, @RequestParam(value = "login", required = true) Long login
-                            ,@RequestParam(value = "password", required = true) Long password){
+    public String loginPost(ModelMap model, @RequestParam(value = "login", required = true) String login
+                            ,@RequestParam(value = "password", required = true) String password){
 
-        return "index";
+        DetachedCriteria employeeCriteria = DetachedCriteria.forClass(Employee.class);
+        employeeCriteria.add(Restrictions.eq("login", login));
+        employeeCriteria.add(Restrictions.sqlRestriction("LIMIT 1"));
+
+        Employee employee = (Employee) persistentService.get(employeeCriteria);
+
+
+
+
+       // UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken();
+
+        authenticationProvider.authenticate(null);
+
+
+        return "redirect:/";
     }
 
     @RequestMapping(value = "out", method = RequestMethod.GET)
